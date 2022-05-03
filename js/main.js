f71e6ff5501d827363069238165897bb8f532bde
Rachel Coster <rachel.coster@outlook.com>
2022-05-03 15:29:16 +1000

    Added animated bees with primitive movement

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 import * as THREE from 'three';
 
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import {FBXLoader} from '../node_modules/three/examples/jsm/loaders/FBXLoader.js';
+import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const clock = new THREE.Clock();
+
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x58ADCB);
 
-//const loader = new FBXLoader(); 
+const loader = new GLTFLoader(); 
 const textureLoader = new THREE.TextureLoader();
+
+var mixers = []; 
 
 const ratio = window.innerWidth / window.innerHeight; 
 var camera = new THREE.PerspectiveCamera(75, ratio, 0.1, 1000);
@@ -28,6 +31,83 @@ var earthSpecMap = textureLoader.load('../Textures/Earth/earthSpecMap.png');
 
 var clouds = textureLoader.load('../Textures/Earth/clouds.png');
 
+var beeTexture = textureLoader.load('../Textures/Bee/bee.png');
+var beeNormalMap = textureLoader.load('../Textures/Bee/beeNormalMap.png');
+var beeAoMap = textureLoader.load('../Textures/Bee/beeAoMap.png');
+var beeSpecMap = textureLoader.load('../Textures/Bee/beeSpecMap.png');
+
+
+//Externalise models
+var bee = new THREE.Object3D(); 
+scene.add(bee);
+
+loader.load('../Models/bee.gltf', (model) => {
+    var anim = model.animations;
+
+    model = model.scene; 
+    model.scale.setScalar(0.15);
+
+    model.traverse((object) => {
+        if(object.isMesh){
+            object.material = new THREE.MeshPhongMaterial({
+                map: beeTexture, 
+                normalMap: beeNormalMap, 
+                aoMap: beeAoMap,
+                specularMap: beeSpecMap,
+                specular: 0xA4A4A4,
+                shininess: 3
+            });
+        }
+    })
+
+    model.position.z = 0.5;
+    model.rotation.x = THREE.MathUtils.degToRad(90);
+    model.rotation.y = THREE.MathUtils.degToRad(45);
+
+
+
+    const animMixer = new THREE.AnimationMixer(model);
+    animMixer.clipAction(anim[1]).play(); 
+    
+    bee.attach(model);
+    mixers.push(animMixer);
+
+    render();
+});
+
+
+loader.load('../Models/bee.gltf', (model) => {
+    var anim = model.animations;
+
+    model = model.scene; 
+    model.scale.setScalar(0.15);
+
+    model.traverse((object) => {
+        if(object.isMesh){
+            object.material = new THREE.MeshPhongMaterial({
+                map: beeTexture, 
+                normalMap: beeNormalMap, 
+                aoMap: beeAoMap,
+                specularMap: beeSpecMap,
+                specular: 0xA4A4A4,
+                shininess: 3
+            });
+        }
+    })
+
+    model.position.z = 0.5;
+    model.position.x = -10;
+    model.rotation.x = THREE.MathUtils.degToRad(90);
+
+
+    const animMixer = new THREE.AnimationMixer(model);
+    animMixer.clipAction(anim[0]).play(); 
+    
+    scene.add(model);
+    mixers.push(animMixer);
+});
+
+
 //Move to a separate file for assets
 const earthGeometry = new THREE.SphereGeometry(5, 32, 32);
 const earthMaterial = new THREE.MeshPhongMaterial({
@@ -39,9 +119,7 @@ const earthMaterial = new THREE.MeshPhongMaterial({
     });
 
 var earth = new THREE.Mesh(earthGeometry, earthMaterial);
-earth.position
-
-scene.add(earth);
+//scene.add(earth);
 
 const cloudGeometry = new THREE.SphereGeometry(5.2, 32, 32);
 const cloudMaterial = new THREE.MeshPhongMaterial({
@@ -51,9 +129,16 @@ const cloudMaterial = new THREE.MeshPhongMaterial({
 });
 
 var clouds = new THREE.Mesh(cloudGeometry, cloudMaterial)
+//earth.add(clouds); 
 
-earth.add(clouds); 
+const mapGeometry = new THREE.BoxGeometry(30, 15, 1); 
+const mapMaterial = new THREE.MeshPhongMaterial({
+    map: earthTexture, 
+    bumpMap: earthBumpMap
+});
 
+var map = new THREE.Mesh(mapGeometry, mapMaterial);
+scene.add(map);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -61,14 +146,22 @@ document.body.appendChild(renderer.domElement);
 
 var controls = new OrbitControls(camera, renderer.domElement);
 
+
 var render = () => {
     renderer.render(scene, camera);
     controls.update();
     
-    var delta = clock.getDelta(); 
-    
-    earth.rotation.y += 0.15 * delta;
-    clouds.rotation.y += 0.05 * delta;
+    var delta = clock.getDelta();
+
+    for(const mixer of mixers) {
+        mixer.update(delta); 
+    }
+
+    bee.position.x += 2 * delta; 
+    bee.position.y -= 2 * delta; 
+
+    //earth.rotation.y += 0.15 * delta;
+    //clouds.rotation.y += 0.05 * delta;
 
     requestAnimationFrame(render); 
 };
